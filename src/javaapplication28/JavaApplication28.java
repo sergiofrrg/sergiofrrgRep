7db2673afa59b98bb1e782477f2b88393ad20929
sergiofrrg <sfg333@hotmail.com>
2014-03-03 11:44:03 +0100

    New sentence in the main function

--- src/javaapplication28/JavaApplication28.java
+++ src/javaapplication28/JavaApplication28.java
@@ -14,6 +14,6 @@ public class JavaApplication28 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola!! Estoy probando el Jenkins");
     }
 }
